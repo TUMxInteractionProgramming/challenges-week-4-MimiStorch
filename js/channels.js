995b246e5b06a7dd84828988e53faebfb89d5f6f
Miriam <miriam.storch@tum.de>
2018-03-21 16:21:16 +0100

    challenge8

---
+++ js/channels.js
@@ -0,0 +1,60 @@
+/**
+Definition der Chats als Variablen, mit den Atrributen:
+    name: string
+    createdOn: Date (year, month, day)
+    createdBy: string
+    starred: Boolean
+    expirsIN: Number (in min)
+    messageCount: Number (of messages left)
+
+**/
+
+/* yummy */
+var yummy = {
+    name: "#Yummy",
+    createdOn: Date(2016, 3, 1),
+    createdBy: "minus.plus.yummy",
+    starred: false,
+    expirsIN: 100,
+    messageCount: 999
+}
+
+/* sevenContinents*/
+var sevenContinents = {
+    name: "#SevenContinents",
+    createdOn: Date(2016, 5, 20),
+    createdBy: "cheeses.yard.applies",
+    starred: true,
+    expirsIN: 77,
+    messageCount: 850
+}
+
+/* killerApp*/
+var killerApp = {
+    name: "#KillerApp",
+    createdOn: Date(2016, 4, 17),
+    createdBy: "limo.reserve.skews",
+    starred: false,
+    expirsIN: 200,
+    messageCount: 50
+}
+
+/* firstPersonOnMars*/
+var firstPersonOnMars = {
+    name: "#FirstPersonOnMars",
+    createdOn: Date(2016, 4, 4),
+    createdBy: "dolphins.jets.clearcut",
+    starred: true,
+    expirsIN: 20,
+    messageCount: 505
+}
+
+/* octoberfest*/
+var octoberfest = {
+    name: "#Octoberfest",
+    createdOn: Date(2016, 5, 25),
+    createdBy: "operated.dice.madness",
+    starred: false,
+    expirsIN: 59,
+    messageCount: 499
+}
--- js/script.js
+++ js/script.js
@@ -5,28 +5,52 @@ console.log("App is alive");
  * #6 #Switcher function for the #channels name in the right app bar
  * @param channelName Text which is set
  */
-function switchChannel(channelName) {
+
+var currentChannel;
+currentChannel = sevenContinents;
+
+var currentLocation = {
+	latitude: 48.249586,
+	longitude: 11.634431,
+	what3words: "shelf.jetted.purple"
+}
+
+function switchChannel(channelObject) {
     //Log the channel switch
-    console.log("Tuning in to channel", channelName);
+    console.log("Tuning in to channel", channelObject);
 
     //Write the new channel to the right app bar
-    document.getElementById('channel-name').innerHTML = channelName;
+    document.getElementById('channel-name').innerHTML = channelObject.name;
 
     //#6 change the #channel #location
-    document.getElementById('channel-location').innerHTML = 'by <a href="http://w3w.co/upgrading.never.helps" target="_blank"><strong>upgrading.never.helps</strong></a>';
+    document.querySelector('#channel-location').innerHTML = 'by <a href="http://w3w.co/' +
+		channelObject.createdBy +
+		'" target="_blank"><strong>' +
+		channelObject.createdBy +
+		'</strong></a>' ;
 
     /* #6 #liking channels on #click */
-    $('#channel-star').attr('src', 'http://ip.lfe.mw.tum.de/sections/star-o.png');
+    $('#channel-star').removeClass('fas far');
+    $('#channel-star').addClass(channelObject.starred ? ' fas' : ' far');
 
     /* #6 #highlight the selected #channel.
        This is inefficient (jQuery has to search all channel list items), but we'll change it later on */
     $('#channels li').removeClass('selected');
-    $('#channels li:contains(' + channelName + ')').addClass('selected');
+    $('#channels li:contains(' + channelObject.name + ')').addClass('selected');
+    
+    currentChannel = channelObject
 }
+
 
 /* #6 #liking a channel on #click */
 function star() {
-    $('#channel-star').attr('src', 'http://ip.lfe.mw.tum.de/sections/star.png');
+   $('#channel-star').toggleClass('far fas');
+    
+    currentChannel.starred = !currentChannel.starred;
+    
+    $('#channels li:contains(' + currentChannel.name +') .fa-star') .removeClass('fas far');
+    $('#channels li:contains(' + currentChannel.name +') .fa-star').addClass(currentChannel.starred ? ' fas' : ' far');
+
 }
 
 /**
@@ -50,4 +74,71 @@ function selectTab(tabId) {
 function toggleEmojis() {
     /* $('#emojis').show(); // #show */
     $('#emojis').toggle(); // #toggle
+}
+
+/** creating new Messages **/
+
+function Message(text) {
+    this.createdBy = currentLocation.what3words;
+    this.latitude = currentLocation.latitude;
+    this.longitude = currentLocation.latitude;
+    this.createdOn = new Date(Date.now());
+    this.expiresOn = new Date(Date.now() + 15 * 60 * 1000);
+    this.text = text;
+    this.own = true;
+}
+
+function createMessageElement(messageObject){
+    var expirseIn = Math.round((messageObject.expiresOn - Date.now()) / 1000 / 60);
+    return '<div class="message' + (messageObject.own ? ' own' : '') + '">' + 
+        '<h3><a href="http://w3w.co/' + messageObject.createdBy + '" target="_blank"><strong>' + messageObject.createdBy + '</strong></a>' +
+        messageObject.createdOn + '<em>' + expirseIn + ' min. left</em></h3>' +
+        '<p>' + messageObject.text + '</p>' + 
+        '<button>+5 min.</button>' +
+        '</div>';
+}
+
+function sendMessage(){
+    var message = new Message($('#message').val());
+    console.log(message);
+    
+    $('#messages').append(createMessageElement(message));
+    
+    $('#messages').scrollTop(100000);
+    
+    $('#message').val('');
+}
+
+
+/* dynamic channels */
+/** create this jQuery:
+*<li onclick="switchChannel(channelObject.name)">
+*<span class="channel-meta">
+*<i class="far fa-star" alt="not starred"></i>
+*<i class="fas fa-chevron-right" alt="select"></i>
+*</span>
+*</li>
+
+*/
+function createChannelElement(channelObject){
+    var channel = $('<li>').text(channelObject.name);
+    var meta = $('<span>').addClass('channel-meta').appendTo(channel);
+    
+    $('<i>').addClass('fa-star').addClass(channelObject.starred ? 'fas' : 'far').appendTo(meta);
+    
+    $('<span>').text(channelObject.expirsIN + ' min').appendTo(meta);
+    $('<span>').text(channelObject.messageCount + ' new').appendTo(meta);
+    
+    $('<i>').addClass('fas fa-chevron-right').appendTo(meta);
+    
+    
+    return channel;
+}
+
+function listChannels(){
+    $('#channels ul').append(createChannelElement(yummy));
+    $('#channels ul').append(createChannelElement(sevenContinents));
+    $('#channels ul').append(createChannelElement(killerApp));
+    $('#channels ul').append(createChannelElement(firstPersonOnMars));
+    $('#channels ul').append(createChannelElement(octoberfest));
 }
